bd8d678720cb69a45a686eae3b0fe7fd0acd6e04
Luis Plazas <lf.plazas10@uniandes.edu.co>
2018-08-05 13:47:25 -0500

    test

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var app = express()
+let app = express();
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-app.get('/test', function (req, res) {
-  res.send('hello world')
+
+
+app.get('/redirect', function (req, res) {
+  res.send(JSON.stringify(req.query))
 })
