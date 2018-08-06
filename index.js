16e6692b4afcfbd5e4cd5944295e081ab1f79d9b
Luis Plazas <lf.plazas10@uniandes.edu.co>
2018-08-05 19:23:07 -0500

    Release

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+const PORT = process.env.PORT || 5000;
+const opn = require('opn');
+let qs = require('qs');
 
 let code = "";
+let access_token = "";
+let old_volume = null;
 
 let app = express();
 app
@@ -10,15 +15,93 @@ app
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .listen(PORT, () => {
+    console.log(`Listening on ${ PORT }`);
+    opn('http://localhost:5000/login');
+  });
 
+app.get('/login', (req, res) => {
+  var scopes = 'user-read-private user-read-currently-playing user-follow-modify user-read-email user-modify-playback-state';
+  res.redirect('https://accounts.spotify.com/authorize' +
+    '?response_type=code' +
+    '&client_id=' + "65fdc90819c849a4a75deeff6c7a2114" +
+    (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+    '&redirect_uri=' + encodeURIComponent("http://localhost:5000/redirect"));
+});
 
-
-app.get('/redirect', function (req, res) {
-	code = req.query.code;
-  res.send(JSON.stringify(req.query))
-})
+app.get('/redirect', (req, res) => {
+  code = req.query.code;
+  getToken(code, res);
+});
 
 app.get('/code', function (req, res) {
   res.send(code)
-})
+});
+
+app.get('/current', function (req, res) {
+
+});
+
+function checkCurrentSong(){
+  let config = {
+    headers: {
+      "Authorization": "Bearer " + access_token,
+      'Content-Type': 'application/json',
+    }
+  };
+  axios.get('https://api.spotify.com/v1/me/player/currently-playing', config)
+    .then(function (response) {
+      if (response.data.item == null) {
+        let exec = require('child_process').exec;
+        if (old_volume == null) {
+          exec('osascript -e \'get volume settings\'', (error, stdout, stderr) => {
+            old_volume = stdout.split(',')[0].split(':')[1];
+          });
+          exec('osascript -e \'set volume 0\'', function callback(e, o, er){});
+        }
+        //Waiting for spotify to release websockets
+        setTimeout(function() {
+          checkCurrentSong();
+        }, 1000);
+      }
+      else{
+        let exec = require('child_process').exec;
+        exec('osascript -e \'set volume '+old_volume+'\'', function callback(e, o, er){});
+        old_volume = null;
+        //Waiting for spotify to release websockets
+        setTimeout(function() {
+          checkCurrentSong();
+        }, 2000);
+      }
+
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function getToken(code, res) {
+  let data = {
+    "code": code,
+    "grant_type": "authorization_code",
+    "redirect_uri": "http://localhost:5000/redirect"
+  };
+  var payload = "65fdc90819c849a4a75deeff6c7a2114" + ":" + "a7be2ccf107640f89e0e93ffbcd7e52d";
+  var encodedPayload = new Buffer(payload).toString("base64");
+  let config = {
+    headers: {
+      "Authorization": "Basic " + encodedPayload,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    }
+  };
+  axios.post('https://accounts.spotify.com/api/token', qs.stringify(data), config)
+    .then(function (response) {
+      access_token = response.data.access_token;
+      res.send("Initialized");
+      checkCurrentSong();
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.send(error.data)
+    });
+}
